b84a13eb3c95b96adbefba011ba32f5c12c527fa
astya965 <astya965@gmail.com>
2020-04-21 08:06:50 +0300

    Add: Carousel methods (start\end scrolling) were added

--- source/js/components/carousel-section/carousel-section.jsx
+++ source/js/components/carousel-section/carousel-section.jsx
@@ -1,36 +1,52 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import Card from "../card/card.jsx";
 import Slider from "infinite-react-carousel";
 
-const CarouselSection = (props) => {
-  const {lootboxes} = props;
+class CarouselSection extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  const settings =  {
-    autoplay: false,
-    autoplayScroll: 2,
-    autoplaySpeed: 10,
-    centerMode: true,
-    initialSlide: 1,
-    slidesToShow: 4,
-    arrowsBlock: false,
-    centerPadding: 0,
-    duration: 300,
+    this.sliderRef = null;
+
+    this.state = {
+      settings: {
+        autoplay: false,
+        autoplayScroll: 2,
+        autoplaySpeed: 10,
+        initialSlide: 1,
+        slidesToShow: 5,
+        arrowsBlock: false,
+        centerPadding: 0,
+        duration: 300,
+      }
+    }
+
+    this._onButtonClick = this._onButtonClick.bind(this);
   };
 
-  return (
-    <section className="carousel-section">
-        <div className="carousel-section__inner">
-          <div className="carousel">
-            <Slider {...settings}>
-              {lootboxes.map((box) => (
-                <Card boxinfo={box} key={box.id}/>
-              ))}
-            </Slider>
+  _onButtonClick() {
+    console.log(1);
+    this.sliderRef.slickPlay();
+
+    setTimeout(() => this.sliderRef.slickPause(), 5500);
+  };
+
+  render() {
+    return (
+      <section className="carousel-section">
+          <div className="carousel-section__inner">
+            <div className="carousel">
+              <Slider {...this.state.settings}  ref={(ele) => {this.sliderRef = ele; window.sliderRef = ele;}}>
+                {this.props.lootboxes.map((box) => (
+                  <Card boxinfo={box} key={box.id}/>
+                ))}
+              </Slider>
+            </div>
           </div>
-        </div>
-        <button className="carousel-section__btn btn" type="button">Открыть</button>
-      </section>
-  );
+          <button className="carousel-section__btn btn" type="button" onClick={this._onButtonClick}>Открыть</button>
+        </section>
+    );
+  }
 }
 
 export default CarouselSection;
