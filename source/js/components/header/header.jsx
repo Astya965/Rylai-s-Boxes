37928f8015d1cb3031980c942fbcaa973914be92
astya965 <astya965@gmail.com>
2020-04-21 06:22:30 +0300

    Add: Header component was added

---
+++ source/js/components/header/header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Header = () => {
+
+  return (
+    <header className="header">
+      <div className="header__logo logo">
+        <img className="logo__img" src="img/logo.png" alt="Любопытная Рилай"/>
+        <p className="logo__text">Любопытно, что в этой коробке!</p>
+      </div>
+      <nav className="header__nav main-nav">
+        <ul className="main-nav__list">
+          <li className="main-nav__item">
+            <a className="main-nav__link" href="#">Главная</a>
+          </li>
+          <li className="main-nav__item">
+            <a className="main-nav__link" href="#">Лидеры</a>
+          </li>
+          <li className="main-nav__item">
+            <a className="main-nav__link" href="#">Арена</a>
+          </li>
+          <li className="main-nav__item">
+            <a className="main-nav__link" href="#">Магазин</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
